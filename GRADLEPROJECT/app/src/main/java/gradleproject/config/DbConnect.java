0f1bb6fe389ccbd6e54f93321ba634ba8f2bf356
aalfm <andialifah351@gmail.com>
2026-05-21 12:06:53 +0800

    BUILD: Gradle dan Database

---
+++ GRADLEPROJECT/app/src/main/java/gradleproject/config/DbConnect.java
@@ -0,0 +1,25 @@
+package gradleproject.config;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
+
+public class DbConnect {
+    private static final String DB_URL = "jdbc:sqlite:db/db_user.db";
+
+    private static Connection connection;
+    private static PreparedStatement preparedStatement;
+    private static ResultSet resultSet;
+
+    public static final void connection() {
+        try {
+            connection = DriverManager.getConnection(DB_URL);
+            System.out.println("Datavase Connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
